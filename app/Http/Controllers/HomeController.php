d90cc386e630843c30c89afde898b7d7c46801ec
Ichsan Firdaus <ichsanfirdaus99@gmail.com>
2016-04-24 11:19:31 +0700

    Add Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,36 +1,97 @@
 <?php namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\API\APIController as API;
 
 class HomeController extends Controller {
-
-	/*
-	|--------------------------------------------------------------------------
-	| Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller renders your application's "dashboard" for users that
-	| are authenticated. Of course, you are free to change or remove the
-	| controller as you wish. It is just here to get your app started!
-	|
-	*/
-
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
+	public function get_Currency()
 	{
-		$this->middleware('auth');
+		$api = new API;
+		$hasil = $api->getCurl('general_api/listCurrency');
+		\App\Currency::whereRaw('id>0')->delete();
+		$data = array();
+		dd($hasil);
+
+		foreach ($hasil->result as $key) {
+			$curr = new \App\Currency;
+			$curr->code = $key->code;
+			$curr->name = $key->name;
+			$curr->save();
+			$data['id'][$curr->id] = $key->code;
+		}
+
+		echo json_decode(
+			array(
+				'status_code'=>200,
+				'insert_data'=>sizeof($data['id'])
+			)
+		);
 	}
 
-	/**
-	 * Show the application dashboard to the user.
-	 *
-	 * @return Response
-	 */
-	public function index()
+	public function get_Lang()
 	{
-		return view('home');
+		$api = new API;
+		$hasil = $api->getCurl('general_api/listLanguage');
+		\App\Lang::whereRaw('id>0')->delete();
+		$data = array();
+		foreach ($hasil->result as $key) {
+			$lang = new \App\Lang;
+			$lang->code = $key->code;
+			$lang->name_long = $key->name_long;
+			$lang->name_short = $key->name_short;
+			$lang->save();
+			$data['id'][$lang->id] = $key->code;
+		}
+
+		echo json_decode(
+			array(
+				'status_code'	=> 200,
+				'inserted_data'	=> sizeof($data['id'])
+			)
+		);
 	}
 
+	public function get_Country()
+	{
+		$api = new API;
+		$hasil = $api->getCurl('general_api/listCountry');
+		\App\Country::whereRaw('id>0')->delete();
+		$data = array();
+		foreach ($hasil->result as $key) {
+			$ctr = new \App\Lang;
+			$ctr->country_id = $key->country_id;
+			$ctr->country_name = $key->country_name;
+			$ctr->country_areacode = $key->country_areacode;
+			$ctr->save();
+			$data['id'][$ctr->id] = $key->code;
+		}
+
+		echo json_decode(
+			array(
+				'status_code'	=> 200,
+				'inserted_data'	=> sizeof($data['id'])
+			)
+		);
+	}
+
+	public function view_Currency()
+	{
+		$s['data'] = \App\Currency::all();
+		return view('master.currency')->with($s);
+	}
+
+	public function view_Lang()
+	{
+		$s['data'] = \App\Lang::all();
+		// echo $data->code;
+		// $data = \App\Lang::all();
+		// die();
+		return view('master.lang')->with($s);
+	}
+
+	public function view_Country()
+	{
+		$s['data'] = \App\Country::all();
+		return view('master.country')->with($s);
+	}
 }
